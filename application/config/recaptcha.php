22ab79877efb19db2dd2e83711bca04b3de78d9c
DediRW <dedi@rumahweb.co.id>
2023-09-23 14:20:20 +0700

    reCAPTCHA v2

--- application/config/recaptcha.php
+++ application/config/recaptcha.php
@@ -5,8 +5,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 // link: http://www.google.com/recaptcha/admin
 // Label : MyreCAPTCHA
 // V2 Invisible
-$config['recaptcha_site_key']   = '6LeynUUoAAAAALPxrJSdgzNrH4FNeTvqOxgZo0ac';
-$config['recaptcha_secret_key'] = '6LeynUUoAAAAAJ1n3HiiaVP5nivuWWl0ZId-q_fz';
+$config['recaptcha_site_key']   = '';
+$config['recaptcha_secret_key'] = '';
 
 // reCAPTCHA supported 40+ languages listed here:
 // https://developers.google.com/recaptcha/docs/language
